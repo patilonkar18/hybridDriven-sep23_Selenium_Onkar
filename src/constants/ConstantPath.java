4f127c6d2e8a972b04c134d61b00f10c11394088
patilonkar18 <patilonkar18@gmail.com>
2023-12-15 00:07:34 +0530

    Onkar_Framework_UntilScreenshots

--- src/constants/ConstantPath.java
+++ src/constants/ConstantPath.java
@@ -5,4 +5,6 @@ public class ConstantPath {
 	public final static String ENV_FILEPATH = ".//config/env.properties";
 	public final static String CHROME_DRIVER_KEY = "webdriver.chrome.driver";
 	public final static String CHROME_DRIVER_VALUE = ".//chromeDriver/chromedriver.exe";
+	public final static int WAIT = 30;
+	public final static int FAST_WAIT = 3;
 }
